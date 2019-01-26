66cec4600586438b107fc2e52cddee6c93280997
Diarmaid de Burca <diarmaid.deburca@everycs.com>
2019-01-26 04:03:27 +0100

    Updating test runner script

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+
+# enable !! command completion
+set -o history -o histexpand
+
+
 pytest_output=$( pytest -q | awk 'FNR==1' | grep F)
 
 if ! [ -z "${pytest_output}" ]; then
-  echo "HERE WE ARE"
+  echo "Unable to commit - tests failed!"
   exit 1
 fi
 
